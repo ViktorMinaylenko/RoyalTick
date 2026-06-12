ae78e4cdd76098cc47456b12c60e848953be1b54
ViktorMinaylenko <ipz224_mvyu@student.ztu.edu.ua>
2026-06-12 21:11:48 +0300

    fix: admin fix

--- shop/app/api/admin/users/route.ts
+++ shop/app/api/admin/users/route.ts
@@ -1,7 +1,7 @@
-
 import clientPromise from '@/lib/mongodb'
 import { getFilteredCollection } from '@/lib/utils/admin-routes'
-
+import { NextResponse } from 'next/server'
+import { corsHeaders } from '@/constants/corsHeaders'
 
 export async function GET(req: Request) {
   try {
@@ -9,6 +9,10 @@ export async function GET(req: Request) {
   } catch (error) {
     throw new Error((error as Error).message)
   }
+}
+
+export async function OPTIONS() {
+  return new NextResponse(null, { ...corsHeaders, status: 200 })
 }
 
 export const dynamic = 'force-dynamic'
